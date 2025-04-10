317335badd06413d985e0edc5bd9dcb0e02fb813
AntiJ0 <firefly175692006!>
2025-04-10 17:53:11 +0900

    Stage1구현, ReturnToMenu 버튼, ExitGame 버튼 구현 및 백그라운드 이미지 삽입

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -17,23 +18,23 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("Respawn"))
-    //    {
-    //        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    //    }
-    //
-    //    if (collision.CompareTag("Finish"))
-    //    {
-    //        collision.GetComponent<LevelObject>().MoveToNextLevel();
-    //    }
-    //
-    //    if (collision.CompareTag("Finish"))
-    //    {
-    //        collision.GetComponent<LevelObject>().MoveToNextLevel();
-    //    }
-    //}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+    
+        if (collision.CompareTag("Finish"))
+        {
+            collision.GetComponent<LevelObject>().MoveToNextLevel();
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
